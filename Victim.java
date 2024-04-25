da3110ec952c3d87075692a1cf646ba89772ba7b
Levi Landers <lee.landers06@gmail.com>
2024-04-25 10:34:51 -0500

    updated the VictimList class to manage the file. Updated the names to include instance data. also updated Victim class to include variables for the number of absences, the score, and the number of picks.

--- Victim.java
+++ Victim.java
@@ -1,15 +1,19 @@
 public class Victim {
+    //File reference string will include name,numAbsent,score,numPicked.
     //Name of the victim
     String name;
-    //Variable that manages the absent system. True if absent.
-    Boolean isAbsent;
+    //Tracks the number of times a person is absent.
+    int numAbsent;
     //Variable that manages a victims score.
     int score;
+    //Tracks the number of times a victim is picked.
+    int numPicked;
 
-    public Victim(String name) {
+    public Victim(String name, int numAbsent, int score, int numPicked) {
         this.name = name;
-        isAbsent = false;
-        score = 0;
+        this.numAbsent = numAbsent;
+        this.score = score;
+        this.numPicked = numPicked;
     }
 
     public String getName() {
@@ -20,19 +24,31 @@ public class Victim {
         this.name = name;
     }
 
-    public Boolean getIsAbsent() {
-        return this.isAbsent;
-    }
-
-    public void setIsAbsent(Boolean absent) {
-        this.isAbsent = absent;
-    }
-
     public int getScore() {
         return this.score;
     }
 
     public void setScore(int score) {
         this.score = score;
+    }
+
+    public int getNumAbsent() {
+        return numAbsent;
+    }
+
+    public void setNumAbsent(int numAbsent) {
+        this.numAbsent = numAbsent;
+    }
+
+    public int getNumPicked() {
+        return numPicked;
+    }
+
+    public void setNumPicked(int numPicked) {
+        this.numPicked = numPicked;
+    }
+
+    public String toString(){
+        return name+"," + String.valueOf(numAbsent) + "," + String.valueOf(score) + "," + String.valueOf(numPicked);
     }
 }
--- VictimList.java
+++ VictimList.java
@@ -1,4 +1,3 @@
-// OLDVolunteerVictimGUI.java
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,24 +7,14 @@ import java.util.*;
 public class VictimList {
     //Master list of names to be selected from.
     List<Victim> listOfNames;
-    //List of Students that are going to be picked from
-    private List<String> allNames;
-    // Set of names of the victims that have already been picked
-    private Set<String> pickedVictims;
-
     //List of all students who are absent
-    List<String> absent;
-    //File where the names come from.
     File file;
 
     public VictimList() {
-        this.listOfNames = new ArrayList<Victim>();
-        this.absent = new ArrayList<String>();
-        this.allNames = new ArrayList<>();
-        this.pickedVictims = new HashSet<>();
+        listOfNames = new ArrayList<Victim>();
         file = new File("Names.txt");
-        try {
-            if (!file.exists()) {
+        try{
+            if(!file.exists()) {
                 throw new FileNotFoundException("File not found: " + file.getAbsolutePath());
             }
         } catch (FileNotFoundException e) {
@@ -36,10 +25,14 @@ public class VictimList {
     //Reads the names from the file, creates Victim objects based on the names
     //Stores the created victims in the listOfNames
     public void importNames() {
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
             String line;
-            while ((line = reader.readLine()) != null) {
-                listOfNames.add(createVictim(line));
+            while((line = reader.readLine()) != null){
+                String[] tempProcessing = line.split(",");
+                int numAbsent = Integer.parseInt(tempProcessing[1]);
+                int score = Integer.parseInt(tempProcessing[2]);
+                int numPicked = Integer.parseInt(tempProcessing[3]);
+                listOfNames.add(createVictim(tempProcessing[0], numAbsent, score, numPicked));
             }
         } catch (FileNotFoundException e) {
             System.out.println("Error: File not found.");
@@ -52,15 +45,15 @@ public class VictimList {
 
     //This method adds a name to the file
     //Then adds the new name to the arrayList of names.
-    public void addNametoFile(String name) {
-        try (FileWriter writer = new FileWriter(file, true);) {
-            writer.write(name);
+    public void addNametoFile(Victim vic) {
+        try (FileWriter writer = new FileWriter(file, true);){
+            writer.write(vic.toString() + "\n");
             System.out.println("Data appended to the file. ");
         } catch (IOException e) {
             System.err.println("Error writing to file: " + e.getMessage());
         }
         //Add name to the arrayList.
-        this.listOfNames.add(createVictim(name));
+        this.listOfNames.add(vic);
     }
 
     //Removes a name from the file
@@ -70,12 +63,12 @@ public class VictimList {
         //Reads the names and writes those names to the dummy file,while searching for a specific name to remove.
         //Once it finds the name, it doesn't write the selected name to a file, and continues reading the file.
         //It then saves the temp file as the names file.
-        try (BufferedReader reader = new BufferedReader(new FileReader(file));
-             BufferedWriter writer = new BufferedWriter(new FileWriter("temp.txt"))) {
+        try(BufferedReader reader = new BufferedReader(new FileReader(file));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("temp.txt"))) {
             String currentLine;
 
             while ((currentLine = reader.readLine()) != null) {
-                if (!currentLine.equals(name.getName())) {
+                if(!currentLine.equals(name.toString())) {
                     writer.write(currentLine);
                     writer.newLine();
                 }
@@ -97,64 +90,32 @@ public class VictimList {
         this.listOfNames.remove(name);
     }
 
-    //Adds a victim to the absent ArrayList.
-    //Also removes them from the master list. (This can be changed if need be)
-    public void updateAbsent(Victim vic) {
-        if (vic.getIsAbsent()) {
-            absent.add(vic.getName());
-            listOfNames.remove(vic);
-        } else {
-            vic.setIsAbsent(true);
-            absent.add(vic.getName());
-            listOfNames.remove(vic);
-        }
-    }
 
-    public String victimPicker() {
-        List<String> possibleChoices = new ArrayList<>(this.allNames);
-        possibleChoices.removeAll(this.pickedVictims);
-        possibleChoices.removeAll(this.absent);
-        if (possibleChoices.isEmpty()) {
-            return null;
-        } else {
-            Collections.shuffle(possibleChoices);
-            String pickedName = (String)possibleChoices.get(0);
-            this.pickedVictims.add(pickedName);
-            return pickedName;
-        }
-    }
-    //Once all victims have been picked you can call this to reset the Victims
-    public void resetList() {
-        this.pickedVictims.clear();
-        this.absent.clear();
-    }
 
     //Displays all the elements of the ArrayList.
     public void displayVictims() {
-        for (Victim vic : listOfNames) {
+        for(Victim vic : listOfNames) {
             System.out.println(vic.getName());
         }
     }
 
-    //Displays all the elements of the absent ArrayList.
-    public void displayAbsent() {
-        for (String vic : absent) {
-            System.out.println(vic);
+    //Returns an ArrayList of strings that contain the victim names.
+    public List<String> getStringList() {
+        List<String> StringList = new ArrayList<String>();
+        for(Victim vic : listOfNames) {
+            StringList.add(vic.getName());
         }
+        return StringList;
     }
 
     //Creates a victim object.
-    public Victim createVictim(String name) {
-        Victim vic = new Victim(name);
+    public Victim createVictim(String name, int numAbsent, int score, int numPicked) {
+        Victim vic = new Victim(name, numAbsent, score, numPicked);
         return vic;
     }
 
-    // Sets all the names into an array list of Strings
-    public void setAllNames(){
-        for (int i = 0; i < this.listOfNames.size(); i++) {
-            this.allNames.add(this.listOfNames.get(i).name);
-        }
+    public Victim getVictim(int index) {
+        return this.listOfNames.get(index);
     }
-
 
 }
